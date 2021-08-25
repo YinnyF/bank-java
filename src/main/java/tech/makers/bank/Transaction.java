bcdfb474ff9633c605c3d8eddb2347e7ddde7e14
cynthia <yinnycfu@gmail.com>
2021-08-25 22:22:26 +0100

    test: Transaction has a running balance

--- src/main/java/tech/makers/bank/Transaction.java
+++ src/main/java/tech/makers/bank/Transaction.java
@@ -2,13 +2,19 @@ package tech.makers.bank;
 import java.time.LocalDate;
 
 public class Transaction {
-    private LocalDate date;
+    private final LocalDate date;
+    private final float balance;
 
-    public Transaction() {
+    public Transaction(float balance) {
         this.date = LocalDate.now();
+        this.balance = balance;
     }
 
     public LocalDate getDate() {
         return date;
+    }
+
+    public float getRunningBalance() {
+        return balance;
     }
 }
--- src/test/java/TransactionTest.java
+++ src/test/java/TransactionTest.java
@@ -1,4 +1,3 @@
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import tech.makers.bank.Transaction;
 import java.time.LocalDate;
@@ -8,11 +7,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TransactionTest {
     @Test
     void testHasADateWhenInitialised() {
-        Transaction subject = new Transaction();
+        Transaction subject = new Transaction(1);
         LocalDate currentDate = LocalDate.now();
-        System.out.println(currentDate);
         assertEquals(currentDate, subject.getDate());
         // perhaps statement should parse the date in the correct format
+    }
+
+    @Test
+    void testHasARunningBalance() {
+        Transaction subject = new Transaction(2000);
+        assertEquals(2000, subject.getRunningBalance());
     }
 
 }
